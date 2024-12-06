43e884e73ffed3ee0a39adb9aa7f97394437d960
Joseph Cocozza <jcocozza47@gmail.com>
2024-12-06 18:31:40 -0500

    feat: python venv setup

--- files/global_shell.sh
+++ files/global_shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# global 
+# global
 # this file is sourced by files like .zshrc and .basrc
 # it provides commands, environment variables i might want access to
 
@@ -18,4 +18,32 @@ alias 'git-tree'='git log --graph --oneline --all'
 # e.g. `ggrepall | fzf`
 ggrepall() {
     git --no-pager branch -a | awk '{print $NF}' | xargs git --no-pager grep -n "$1"
+}
+
+
+# a command that will install the tools that makes vim + python nice based on my setup
+python_venv_setup() {
+    ask_yes_no() {
+        echo "virtual environment path is: $VIRTUAL_ENV"
+        echo -n "is this the virtual environment you'd like to install packages in? (y/n): "
+        read choice
+        # Check the input
+        case "$choice" in
+        [Yy]* ) return 0;;
+        [Nn]* ) return 1;;
+        * ) echo "Invalid input, please enter y or n."; ask_yes_no;; # Reask if input is invalid
+      esac
+    }
+
+    if [ -z "$VIRTUAL_ENV" ]; then
+        echo "no python virtual environment is active. please activate."
+    else
+        ask_yes_no
+        if [ $? -eq 0 ]; then
+            echo "installing to $VIRTUAL_ENV..."
+            pip install flake8 mypy black python-lsp-server
+        else
+            echo "please switch to the virtual environment you'd like to use."
+        fi
+    fi
 }
