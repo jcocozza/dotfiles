92aed5a034fee4c0d2ae665a7f89220e518deac5
Joseph Cocozza <jcocozza47@gmail.com>
2025-05-24 20:06:57 -0400

    fix: need to return to repo after symlinks

--- update.sh
+++ update.sh
@@ -43,6 +43,7 @@ for file in $(ls -A "${DOTFILES_PATH}"); do
     ln -s "${DOTFILES_PATH}/$file" "$file"
 done
 
+cd $REPO_PATH
 echo "********** ignoring future changes in local files **********"
 git update-index --skip-worktree "${DOTFILES_PATH}/local_shell.sh"
 git update-index --skip-worktree "${DOTFILES_PATH}/local_vimrc.vim"
